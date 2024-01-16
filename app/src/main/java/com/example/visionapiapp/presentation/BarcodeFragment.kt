848d10362eeee54eb2ea9f75df28ef0722fdf2d4
Laryssa Fagundes <laryssa.agomesfagundes@gmail.com>
2024-01-16 13:40:54 -0300

    feat: configured viewbinding and navigation to scanner barcode screen

--- app/src/main/java/com/example/visionapiapp/presentation/BarcodeFragment.kt
+++ app/src/main/java/com/example/visionapiapp/presentation/BarcodeFragment.kt
@@ -1,10 +1,25 @@
 package com.example.visionapiapp.presentation
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.visionapiapp.databinding.FragmentBarcodeBinding
 
 class BarcodeFragment: Fragment() {
+    private lateinit var binding: FragmentBarcodeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentBarcodeBinding.inflate(layoutInflater)
+        return binding.root
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 }
--- app/src/main/java/com/example/visionapiapp/presentation/HomeFragment.kt
+++ app/src/main/java/com/example/visionapiapp/presentation/HomeFragment.kt
@@ -1,12 +1,37 @@
 package com.example.visionapiapp.presentation
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.visionapiapp.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var binding: FragmentHomeBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpListeners()
+    }
+
+    private fun setUpListeners(){
+        binding.apply {
+            buttonOpenBarcodeScanner.setOnClickListener {
+                findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToBarcodeFragment())
+            }
+        }
+    }
+
 }
